02284e21351fc42bfe935339ab86582932cf77b6
InsaneStudent1998 <106678831+1998ky262@users.noreply.github.com>
2022-12-02 22:29:18 +0900

    Update Sine2.js

--- Sine2.js
+++ Sine2.js
@@ -1,6 +1,6 @@
 setInterval(changehtml,100);
 let sine="死ね";
 function changehtml(){
-  document.querySelector('html').innerHTML = `<body><div style="color:#00e63a;">${sine}</div></body>`;
+  document.querySelector('html').innerHTML = `<body style="background-color:black;"><div style="color:#00e63a;">${sine}</div></body>`;
   sine=sine+"死ね";
 }
