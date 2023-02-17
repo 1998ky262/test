7d41d7c12537d044360cdf736310a57bec21778c
1998 <106678831+1998ky262@users.noreply.github.com>
2023-02-17 11:15:24 +0900

    Colaboratory を使用して作成しました

---
+++ Untitled0.py
@@ -0,0 +1,55 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOxqaErMY/2u7t1R/Ks3mQx",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/1998ky262/test/blob/main/Untitled0.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 4,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "yyzOJLjNJGkg",
+        "outputId": "989c334b-501e-4fc3-d539-ae4c1a05e23d"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "114514\n"
+          ]
+        }
+      ],
+      "source": [
+        "def wanko (NTR):\n",
+        "  print(NTR)\n",
+        "wanko(114514)"
+      ]
+    }
+  ]
+}
